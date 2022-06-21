40a6dd68888eabd42f57c410e8a3a817eb68b3b9
Soezdemir23 <Soezdemir3423@gmail.com>
2022-06-21 14:31:23 -0600

    Created dist files from running webpack

---
+++ main.bundle.js
@@ -0,0 +1,20 @@
+"use strict";
+(self["webpackChunktodo"] = self["webpackChunktodo"] || []).push([["main"],{
+
+/***/ "./src/index.ts":
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+/***/ (() => {
+
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.ts"));
+/******/ }
+]);
