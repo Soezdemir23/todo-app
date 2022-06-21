15d3278ab672f429bf6184ac06682f30ea7fa829
Soezdemir23 <Soezdemir3423@gmail.com>
2022-06-21 15:34:25 -0600

    Rudimentary todo and project class building; configuring webpack

--- main.bundle.js
+++ main.bundle.js
@@ -8,6 +8,7 @@
 /***/ (() => {
 
 
+console.log("Hello World");
 
 
 /***/ })
@@ -18,3 +19,4 @@
 /******/ var __webpack_exports__ = (__webpack_exec__("./src/index.ts"));
 /******/ }
 ]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7OztBQUFBLE9BQU8sQ0FBQyxHQUFHLENBQUMsYUFBYSxDQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kby8uL3NyYy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIkhlbGxvIFdvcmxkXCIpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
