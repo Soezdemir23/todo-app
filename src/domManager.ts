6c5c53422c56c175e93b9286e9babc137e5f173b
Soezdemir23 <Soezdemir3423@gmail.com>
2022-06-26 02:39:09 -0600

    Finish initial layout rework

--- src/domManager.ts
+++ src/domManager.ts
@@ -1,3 +1,4 @@
 export function readTask() {
-    
+    let addTaskContainer = document.getElementById("add-task-container")?.querySelector("input")
+    return addTaskContainer
 }
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@ import { Todo } from "./todos"
 import { Project} from "./projects"
 import './sass/style.scss'
 import { hookToElements } from "./hook_assets"
+import { readTask } from "./domManager"
 console.log("Hello World")
 
 let todo = new Todo(
     "Write proejct"
 )
 hookToElements()
+
+readTask()
+
 
 console.log(todo.info())
 
