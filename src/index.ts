b3e959ab09d9b4d903a7533ebdcf536c3cbe1491
Soezdemir23 <Soezdemir3423@gmail.com>
2022-08-01 10:51:50 -0600

    refactor: check if my Day exists

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { Todo } from "./todos"
-import { Project} from "./projects"
+import { ProjectList } from "./projects"
 import './sass/style.scss'
 import { DOMManager } from "./domManager"
 import { StorageManaging } from "./manager"
-import { addDays, compareAsc, format} from 'date-fns'
 
 // get 
 
@@ -19,5 +18,78 @@ dom.readTaskFromDOM()
 dom.listContext()
 dom.formContext()
 dom.redrawSubmenus()
+dom.ProjectListMenu()
 //dom.populateSubtask()
 // this was instantiated and since the task variable was zero, it didn't work so well
+if (localStorage.getItem("My Day") === null) {
+    localStorage.setItem("My Day", JSON.stringify({
+        "name": "My Day",
+        "todos": [
+            {
+                "title": "coralla",
+                "description": "This is the first todo",
+                "dueDate": "2022-08-06T11:16:38.937Z",
+                "priority": 3,
+                "done": false,
+                "checklist": {},
+                "repeat": 1,
+                "repeatDate": "2022-07-31T00:38:54.094Z",
+                "cycle": [
+                    "8th August 2022",
+                    "10th August 2022",
+                    "10th October 2022",
+                    "28th August 2022"
+                ],
+                "notes": "We can write notes for you"
+            },
+            {
+                "title": "corallb",
+                "description": "This is the second todo",
+                "dueDate": "2022-08-06T11:16:38.937Z",
+                "priority": 3,
+                "done": false,
+                "checklist": {},
+                "repeat": 1,
+                "repeatDate": "2022-07-31T00:38:54.094Z",
+                "cycle": [
+                    "8th August 2022",
+                    "10th August 2022",
+                    "10th October 2022",
+                    "28th August 2022"
+                ]
+            }, {
+                "title": "corallc",
+                "description": "This is the third todo",
+                "dueDate": "2022-08-06T11:16:38.937Z",
+                "priority": 3,
+                "done": false,
+                "checklist": {},
+                "repeat": 1,
+                "repeatDate": "2022-07-31T00:38:54.094Z",
+                "cycle": [
+                    "8th August 2022",
+                    "10th August 2022",
+                    "10th October 2022",
+                    "28th August 2022"
+                ]
+            },
+            {
+                "title": "coralld",
+                "description": "this is the fourth todo",
+                "dueDate": "2022-08-06T11:16:38.937Z",
+                "priority": 3,
+                "done": false,
+                "checklist": {},
+                "repeat": 1,
+                "repeatDate": "2022-07-31T00:38:54.094Z",
+                "cycle": [
+                    "8th August 2022",
+                    "10th August 2022",
+                    "10th October 2022",
+                    "28th August 2022"
+                ]
+            }
+        ]
+    })
+    )
+}
